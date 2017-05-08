8d16e0febc848deb86c6fb7805b221ff6898c3a8
Brett Schules <brettschules@gmail.com>
2017-05-08 12:26:33 -0400

    Done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+  num.times.select { |num|   (num % 3 == 0 || num % 5 == 0) && (num !=0) }
+end
+
+def sum_multiples(limit)
+  limit.times.select { |num|   (num % 3 == 0 || num % 5 == 0) && (num !=0) }.inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  attr_reader :num
+
+  def num=(num)
+    if num == nil
+      raise ArgumentError
+    else
+      @num = num
+    end
+  end
+
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    @num.times.select { |num|   (num % 3 == 0 || num % 5 == 0) && (num !=0) }
+  end
+
+  def sum_multiples
+    @num.times.select { |num|   (num % 3 == 0 || num % 5 == 0) && (num !=0) }.inject(:+)
+  end
+end
